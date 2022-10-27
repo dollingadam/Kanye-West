34cbb7c45c280935eb8b3fcaa91d6b1f6265872d
dollingadam <adam.dolling1@gmail.com>
2022-10-27 11:10:26 +0200

    fetching api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hej')
+let random = () => {
+fetch('https://api.kanye.rest/')
+.then (response => response.json())
+.then (data => {
+    console.log(data)
+
+
+document.querySelector("#quotes").innerHTML = data.quote;
+})}
+random()
